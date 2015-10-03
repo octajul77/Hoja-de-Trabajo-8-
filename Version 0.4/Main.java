98106106595d6d77509aa428fe11a6d9b23ab7e6
DrFreddy <fre_rz@hotmail.com>
2015-10-03 14:01:10 -0500

    Main comentado e interfaz amigable

--- Version 0.4/Main.java
+++ Version 0.4/Main.java
@@ -1,4 +1,18 @@
-//Julio Gonzalez 14096
+
+/**
+ * Universidad del Valle de Guatemala
+ * Algortimos y estrcutura de datos
+ * Seccion 10- Hoja de Trabajo 2
+ * -------------------------------------------------
+ * Laboratorio #8
+ * @author Freddy Ruíz 14592/ Julio Ochoa 14096
+ * -------------------------------------------------
+ *Main:
+ *Esta clase es la encargada de interactuar con el usuario y mostrarle
+ *el orden en el cual serán ordenados los datos.
+ * 
+ */
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -6,25 +20,27 @@ import java.io.IOException;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		
-		//Se crea el vector heap para guardar a cada paciente
+		System.out.println ("------BIENVENIDO AL HOSPITAL VIDA MEDICA--------");
+		/**
+		 * Inicialización del vectorHeap y otras variables para el almacenamiento de pacientes
+		 * 
+		 **/
 		VectorHeap<Paciente> emergencias = new VectorHeap<Paciente>();
-		
-		//Se lee el archivo 
-		FileReader fileReader = new FileReader("pacientes.txt");
+		int contadorPacientes= 0;  
+		String infoPaciente; //String que almacena la informacón  del paciente
+		String[] clasificacionInfo; //array donde se guarda separada la informacion 
+		Paciente temporal; //Se crea un objeto temporal tipo Patient
+			
+		/**
+		 * Lectura del archivo 
+		 **/ 
+		FileReader fileReader = new FileReader("src\\pacientes.txt");
 		BufferedReader lector = new BufferedReader(fileReader);
 		
-		//contador
-		int contadorPacientes= 0;
-		//Stirng guarda linea de info del .txt
-		String infoPaciente;
-				
-		//array donde se guarda separada la informacion 
-		String[] clasificacionInfo;
 		
-		
-		
-		//Ciclo donde se lee el archivo linea por linea
+		/**
+		 * Ciclo mediante el cual será leída cada una de las líneas del  archivo
+		 */
 		while((infoPaciente = lector.readLine())!=null){
 			//Se hace la separacion de informacion
 			clasificacionInfo = infoPaciente.split(", ");
@@ -34,15 +50,16 @@ public class Main {
 			contadorPacientes++;
 		}
 		
-		
-		
-		//Se cierra el lector
+		//Cierre del lector
 		lector.close();
 		
-		//Se crea un objeto temporal tipo Patient
-		Paciente temporal;
-		
-		//Ciclo de impresion de pacientes a partir del hospital (VectorHeap)
+
+		/**
+		 * Ciclo mediante el cual será impreso en orden de prioridad la atención de clientes
+		 * 
+		 */
+		System.out.println("\nA continuación se mostrará el orden en el que serán \natendidos los pacientes:");
+		System.out.println("\nOrden Correcto:");
 		for(int i=0; i<contadorPacientes; i++){
 			temporal = emergencias.remove();
 			System.out.println("Nombre: "+temporal.getNombre()+", Causa: "+temporal.getSintoma()+", Prioridad: "+temporal.getPrioridad());

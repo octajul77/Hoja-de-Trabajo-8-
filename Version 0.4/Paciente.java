166716d41bf5fe845d07e6a2e612c925dfb854f7
DrFreddy <fre_rz@hotmail.com>
2015-10-03 14:05:57 -0500

    Update Paciente.java

--- Version 0.4/Paciente.java
+++ Version 0.4/Paciente.java
@@ -1,17 +1,49 @@
-//Julio Gonzalez 14096
+/**
+ * Universidad del Valle de Guatemala
+ * Algortimos y estrcutura de datos
+ * Seccion 10- Hoja de Trabajo 2
+ * -------------------------------------------------
+ * Laboratorio #8
+ * @author Freddy Ruíz 14592/ Julio Ochoa 14096
+ * -------------------------------------------------
+ *Paciente:
+ *Esta clase es la encargada de crear objetos de tipo paciente
+ *con su nombre, sintoma y prioridad para poder determinar
+ *el orden en el cual serán atendidos
+ * 
+ */
 public class Paciente implements Comparable<Paciente> { 
     
+    //Inicializaicón de variables
     private String nombre;
     private String prioridad;
     private String sintoma;
 
-	public Paciente(String nombre, String sintoma, String prioridad) {
+  /**
+   * Método: Paciente (Constructor)
+   * Funcionalidad:
+   * Crear un nuevo objeto de tipo paciente.
+   * @param nombre, sintoma, prioridad
+   * */
+     public Paciente(String nombre, String sintoma, String prioridad) {
 		super();
 		this.nombre = nombre;
 		this.sintoma = sintoma;
 		this.prioridad = prioridad;
 	}	
-	
+
+//Setters y Getters	
+public void setNombre(String nombre) { 
+        this.nombre = nombre;
+    }
+
+    public void setPrioridad(String prioridad) {
+        this.prioridad = prioridad;
+    }
+
+    public void setSintoma(String sintoma) {
+        this.sintoma = sintoma;
+    }
     public String getNombre() {
         return nombre;
     }
@@ -24,19 +56,15 @@ public class Paciente implements Comparable<Paciente> {
         return prioridad;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public void setPrioridad(String prioridad) {
-        this.prioridad = prioridad;
-    }
-
-    public void setSintoma(String sintoma) {
-        this.sintoma = sintoma;
-    }
-    
-  
+    //Implemetación de la clase compareTo
+    /**
+     * Método: compareTo
+     * Funcionalidad:
+     * Este método está encargado de comprar los pacientes y determinar cuál tiene mayor prioridad de
+     * atención
+     * @param otroPaciente 
+     * @return paciente con mayor prioridad
+     * */
 	public int compareTo(Paciente otroPaciente) {
 		return prioridad.compareTo(otroPaciente.getPrioridad());
 	}
